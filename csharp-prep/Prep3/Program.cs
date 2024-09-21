e15bfc540f082c4a87fb3c1ae8f2bcc4f5a03549
ttrampp <ttrampp@msn.com>
2024-09-21 12:04:25 -0600

    Core Requirements met

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Formats.Tar;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Next three lines of comments were for Core Requirements 1 & 2.
+        //Console.Write("What is the magic number? ");
+        //string magicNumber = Console.ReadLine();
+        //int mN = int.Parse(magicNumber);
+
+        //Core Requirement 3--Random number generated for magic number
+        Random randomGenerator = new Random();
+        int mN = randomGenerator.Next(1, 1001);
+
+        int guess = -1;
+
+        do
+        {
+            Console.Write($"My guess is: ");
+            guess = int.Parse(Console.ReadLine());
+            //int guessNum = int.Parse(guess);
+
+            if (guess > mN)
+            {
+            Console.WriteLine("Lower");
+            }
+
+            else if (guess < mN)
+            {
+            Console.WriteLine("Higher");
+            }
+
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+        } while (guess != mN);
+        
+        
     }
 }
