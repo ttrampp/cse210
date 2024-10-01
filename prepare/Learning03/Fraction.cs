e7167a6c31b4d509c715316cb33763e28e90c3b8
ttrampp <ttrampp@msn.com>
2024-09-30 21:23:27 -0600

    changed obj/property mapping

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _top = numerator;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string numbers = $"{_top}/{_bottom}";
+        return numbers;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }  
+
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstC = new Fraction();
+        Console.WriteLine(firstC.GetFractionString());
+        Console.WriteLine(firstC.GetDecimalValue());
+
+        Fraction secondC = new Fraction(5);
+        Console.WriteLine(secondC.GetFractionString());
+        Console.WriteLine(secondC.GetDecimalValue());
+
+        Fraction thirdC = new Fraction(3,4);
+        Console.WriteLine(thirdC.GetFractionString());
+        Console.WriteLine(thirdC.GetDecimalValue());
+
+        Fraction fourthC = new Fraction(1,3);
+        Console.WriteLine(fourthC.GetFractionString());
+        Console.WriteLine(fourthC.GetDecimalValue());
     }
 }
