4fea6dbfd14b574bc4a5ef09e909bcf7e46a00c9
ttrampp <ttrampp@msn.com>
2024-10-15 08:10:08 -0600

    cleaned up code, removed unnecessary comments

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -7,20 +7,14 @@ namespace ScriptureMem
 {
     public class Scripture
     {
-        private Reference _reference;                               //_reference not used
+        private Reference _reference;                             
         private List<Word> _words;
 
-        public Scripture(Reference reference, string text)          //never called
+        public Scripture(Reference reference, string text)          
         {
             _reference = reference;
-            /////_words = [];
-            _words = text.Split(' ').Select(word => new Word(word)).ToList();
 
-            /////List<string> splitText = text.Split(" ").ToList();               //make a new list full of strings, call it splitText
-            /////foreach (string word in splitText)
-            /////{
-            /////    _words.Add(new Word(word));
-            /////}
+            _words = text.Split(' ').Select(word => new Word(word)).ToList();
 
         }
 
@@ -30,13 +24,7 @@ namespace ScriptureMem
         }
         public string GetDisplayText()
         {
-            /////string displayText = "";
 
-            /////foreach (Word word in _words)
-            /////{
-            /////    displayText += word.GetDisplayText() + " ";
-            /////}
-            /////return displayText.Trim();
             return _reference.GetDisplayReference() + " " + string.Join(" ", _words.Select(w => w.GetDisplayText()));
         }
 
@@ -54,32 +42,11 @@ namespace ScriptureMem
                 visibleWords.RemoveAt(index);
             }
             
-            
-            /////int hiddenCount = 0;
-
-            /////while (hiddenCount < numToHide)
-            /*{
-                int randomIndex = randomWord.Next(_words.Count);
-
-                if (!_words[randomIndex].IsHidden())
-                {
-                    _words[randomIndex].Hide();
-                    hiddenCount++;
-                }
-            }*/
         }
 
         public bool IsCompletelyHidden()
         {
             return _words.All(w => w.IsHidden());
-            /*foreach (Word word in _words)
-            {
-                if (!word.IsHidden())
-                {
-                    return false;
-                }
-            }
-            return true;*/
         }
 
     }
